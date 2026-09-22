a412b7ddc3736d6c8430473e838ec3ed471c24ed
BursaliAlperen <bursali.alperen.131@gmail.com>
2026-09-22 11:46:09 +0300

    Add ScriptVault loader

---
+++ main.lua
@@ -0,0 +1,3 @@
+-- ScriptVault loader
+local KEY_SYSTEM_URL = "https://raw.githubusercontent.com/BursaliAlperen/SAE-AUTO-SCR-PT/main/keysystem.lua"
+return loadstring(game:HttpGet(KEY_SYSTEM_URL))()

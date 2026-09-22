7e3c1b6f652cce185cc23b4cf4eef9e147531034
BursaliAlperen <bursali.alperen.131@gmail.com>
2026-09-22 12:28:54 +0300

    Add Steal an Egg 6.2 configuration module

---
+++ games/steal_an_egg/config.lua
@@ -0,0 +1,10 @@
+-- ScriptVault Steal an Egg 6.2 configuration
+return {
+    VERSION = "6.2.0",
+    GAME = "Steal an Egg",
+    PRIMARY_PLACE_IDS = {107778070777162},
+    REMOTE_COOLDOWN = 0.35,
+    FARM_TICK = 0.25,
+    ESP_TICK = 0.5,
+    HTTP_BASE = "https://raw.githubusercontent.com/BursaliAlperen/SAE-AUTO-SCR-PT/main/",
+}
